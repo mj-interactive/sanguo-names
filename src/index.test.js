e6a35a97bb6ea86df56f1b303aad48115e1b80f1
Jie Li <jie.li@mj-interactive.com>
2016-05-06 19:56:09 -0400

    setup unit testing with mocha and chai

---
+++ src/index.test.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect;
+var sanguoName = require('./index.js');
+
+describe('sanguo-names', function() {
+	it('should work!', function() {
+		expect(true).to.be.true;
+	})
+})
