df2b6f3c7c4d6d25a48d3a384dcc1efece3e6aa7
Jie Li <jie.li@mj-interactive.com>
2016-05-06 22:49:30 -0400

    add unit testing

--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,29 @@
 var expect = require('chai').expect;
-var sanguoName = require('./index.js');
+var sanguoNames = require('./index');
 
 describe('sanguo-names', function() {
-	it('should work!', function() {
-		expect(true).to.be.true;
-	})
+
+	describe('all', function() {
+		it('should return an array of strings', function() {
+			expect(sanguoNames.all).to.satisfy(isArrayOfStrings);
+
+			function isArrayOfStrings(array) {
+				return array.every(function(item) {
+					return typeof item === 'string';
+				});
+			}
+		});
+
+		it('should always contain Zhao Yun', function () {
+			expect(sanguoNames.all).to.include('Zhao Yun');
+		});
+	});
+
+	describe('random', function () {
+		it('should return a random name from the sanguoNames.all', function () {
+			var randomName = sanguoNames.random();
+			expect(sanguoNames.all).to.include(randomName);
+		});
+	});
+
 })
